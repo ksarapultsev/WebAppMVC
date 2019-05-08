0d84d4cb6b19c4f53b930a487752eeca41ef45a5
ksarapultsev <ksarapultsev2014@gmail.com>
2019-05-08 09:23:19 +0600

    Day 1

--- WebAppMVC/Controllers/HomeController.cs
+++ WebAppMVC/Controllers/HomeController.cs
@@ -1,16 +1,60 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WebAppMVC.Models;
+using System.Net.Http;
+using System.Data.Entity;
 
 namespace WebAppMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var userName = await GetUserName();
+            var user = await GetUser(userName);
+            if (user == null)
+            {
+               user =  await AddUser(new User { UserName = userName });
+            }
+                
+            return View(user);
+        }
+
+        private async Task<User> GetUser(string userName)
+        {
+            User result = null;
+            using (var appContext = new AppContext())
+            {
+                result = await appContext.Users.FirstOrDefaultAsync(f => f.UserName.ToLower() == userName.ToLower());
+            }
+            return result;
+        }
+
+        public async Task<User> AddUser(User user)
+        {
+            using (var appContext = new AppContext())
+            {
+                appContext.Users.Add(user);
+                await appContext.SaveChangesAsync();
+            }
+            return user;
+        }
+
+        private async Task<string> GetUserName()
+        {
+            var userName = string.Empty;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri("http://localhost:4172/");
+                var response = await client.GetAsync("api/app/GetUserName");
+                userName = response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync() : string.Empty;
+            }
+                return userName;
         }
 
         public ActionResult About()
---
+++ WebAppMVC/Models/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVC.Models
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
